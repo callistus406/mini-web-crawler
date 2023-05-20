0e180ef5b8a2ea7ff257c60216173d9427cfecd9
callistus406 <callistus406@gmail.com>
2023-05-20 14:16:56 +0100

    added single crawler

--- main/crawler.js
+++ main/crawler.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom');
+
 const normalizeURL = (input) => {
   const urlObj = new URL(input);
 
@@ -9,4 +11,76 @@ const normalizeURL = (input) => {
 
 // console.log(normalizeURL('http://capacitybay.org'));
 
-module.exports = { normalizeURL };
+const fetchUrlFromHtmlBody = (html, baseURL) => {
+  const urls = [];
+  // put the html in a object
+
+  const dom = new JSDOM(html);
+  // get all anchor tags
+  const anchorTags = dom.window.document.querySelectorAll('a');
+
+  for (const anchorTag of anchorTags) {
+    if (anchorTag.href.slice(0, 1) === '/') {
+      try {
+        urls.push(new URL(anchorTag.href, baseURL).href);
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else {
+      try {
+        urls.push(new URL(anchorTag.href).href);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  return urls;
+};
+
+console.log(
+  fetchUrlFromHtmlBody(
+    `
+<html>
+<body>
+<a href="http://capacitybay.org">
+home
+</a>
+<a href="http://capacitybay.org/about">
+about
+</a>
+<a href="/contact">
+contact
+</a>
+</body>
+</html>
+`,
+    'http://capacitybay.org'
+  )
+);
+
+const crawlPage = async (baseURL, currentURL, pages) => {
+  console.log(`crawling: ${currentURL}`);
+
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(baseURL);
+
+  try {
+    const response = await fetch(currentURL);
+
+    const contentType = response.headers.get('content-type');
+    if (!contentType.includes('text/html')) {
+      console.log('non-html response:' + contentType);
+      return;
+    }
+
+    const result = await response.text();
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  console.log(anchorTags);
+};
+
+module.exports = { normalizeURL, fetchUrlFromHtmlBody };
+crawlPage('http://blog.boot.dev');
--- tests/crawler.test.js
+++ tests/crawler.test.js
@@ -1,5 +1,5 @@
 const { test, expect, describe } = require('@jest/globals');
-const { normalizeURL } = require('../main/crawler');
+const { normalizeURL, fetchUrlFromHtmlBody } = require('../main/crawler');
 describe('test block for URL formatting', () => {
   // this test checks for protocol in url
   test('normalize protocol ', () => {
@@ -31,6 +31,68 @@ describe('test block for URL formatting', () => {
     const testInput = 'http://CApacitybay.org/link';
     const result = normalizeURL(testInput);
     const expectedOutput = 'capacitybay.org/link';
+    expect(result).toEqual(expectedOutput);
+  });
+});
+
+const html = `
+<html>
+<body>
+<a href="http://capacitybay.org">
+home
+</a>
+<a href="http://capacitybay.org/about">
+about
+</a>
+<a href="/contact">
+contact
+</a>
+</body>
+</html>
+`;
+
+describe('test block for normalizing html links', () => {
+  test('test absolute urls and crawled page', () => {
+    const testInputUrl = 'http://CApacitybay.org/link';
+    const testInputHtml = html;
+    const result = fetchUrlFromHtmlBody(testInputHtml, testInputUrl);
+    const expectedOutput = [
+      'http://capacitybay.org/',
+      'http://capacitybay.org/about',
+      'http://capacitybay.org/contact',
+    ];
+    expect(result).toEqual(expectedOutput);
+  });
+  //   ======================================================
+  test('test relative urls and crawled page', () => {
+    const testInputUrl = 'http://Capacitybay.org/link';
+    const testInputHtml = html;
+    const result = fetchUrlFromHtmlBody(testInputHtml, testInputUrl);
+    const expectedOutput = [
+      'http://capacitybay.org/',
+      'http://capacitybay.org/about',
+      'http://capacitybay.org/contact',
+    ];
+    expect(result).toEqual(expectedOutput);
+  });
+  //   ==================================================
+  test('test both  url pattern and crawled page', () => {
+    const testInputUrl = 'http://Capacitybay.org/link';
+    const testInputHtml = html;
+    const result = fetchUrlFromHtmlBody(testInputHtml, testInputUrl);
+    const expectedOutput = [
+      'http://capacitybay.org/',
+      'http://capacitybay.org/about',
+      'http://capacitybay.org/contact',
+    ];
+    expect(result).toEqual(expectedOutput);
+  });
+  //   ===================================================
+  test('test to handle errors', () => {
+    const testInputUrl = 'http://Capacitybay.org/link';
+    const testInputHtml = `<html><body> <a href="app/pop"></a></body></html>`;
+    const result = fetchUrlFromHtmlBody(testInputHtml, testInputUrl);
+    const expectedOutput = [];
     expect(result).toEqual(expectedOutput);
   });
 });
